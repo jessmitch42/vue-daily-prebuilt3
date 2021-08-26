16cdac3b5f6015cf5b765e9f0dde5c117de6dd26
Jessica Mitchell <jess@pluot.co>
2021-08-26 15:21:59 -0400

    update post request

--- src/api.js
+++ src/api.js
@@ -1,6 +1,4 @@
 async function createRoom() {
-  const apiKey = process.env.VUE_APP_DAILY_API_KEY;
-  if (!apiKey) return null;
   // we'll add 30 min expiry (exp) so rooms won't linger too long on your account
   // we'll also turn on chat (enable_chat)
   // see other available options at https://docs.daily.co/reference#create-room
@@ -15,10 +13,6 @@ async function createRoom() {
   const response = await fetch(`${window.location.origin}/api/rooms`, {
     method: "POST",
     body: JSON.stringify(options),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + apiKey,
-    },
   });
   const room = await response.json();
   return room;
